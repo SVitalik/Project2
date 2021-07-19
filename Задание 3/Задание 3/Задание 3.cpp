2825f4077745f5a6da889cf114669c2d0b0edbdf
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:53:08 +0300

    Add files via upload

---
+++ Задание 3/Задание 3/Задание 3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a, b, c, d;
+	int a3, a2, a1;
+	int b2, b1;
+	cout << "Введите трёхзначное число:" << endl;
+	cin >> a;
+	cout << "Введите двухзначное число:" << endl;
+	cin >> b;
+	cout << "Ответ ";
+	a1 = a % 10;
+	a2 = (a % 100 - a1) / 10;
+	b1 = b % 10;
+	c = a1 + b1;
+	cout << c << "  " << endl;
+
+
+
+}
---
+++ Задание 4/Задание 4/Задание 4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(void)
+
+{
+    setlocale(LC_ALL, "RUS");
+    int N;
+    int X;
+    int c1, c2, c3;
+    cout << "Введите трёхзначное число: ";
+    cin >> N;
+    cout << "Ответ " << endl;
+    c1 = N % 10;
+    c2 = (N % 100 - c1) / 10;
+    c3 = (N - N % 100) / 100;
+    if (N > 99) { X = c1 * 10 + c2 * 100 + c3; }
+
+    if ((N > 9) && (N < 100)) { X = c1 * 10 + c2; }
+    if (N < 10) { X = c1; }
+    cout << X << "  " << endl;
+}
+
