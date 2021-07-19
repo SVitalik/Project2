65a11547039ffb893041bb468acf2fd27760b355
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:51:34 +0300

    Add files via upload

---
+++ Практическая 3.2/Практическая 3.2/Практическая 3.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+void main()
+{
+    system("cls");
+    setlocale(LC_ALL, "RUS");
+    double x, a, b, y;
+    cout << "Введите значение x,a,b: " << endl;
+    cout << "x = ";
+    cin >> x;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    y = (a * x + b) / (pow(log((b * x)), 2)) + sqrt(a);
+    cout << "Результат y = " << y << endl;
+    int ch = _getch();
+}
