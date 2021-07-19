0508ee96aa33ec094232fc178601a39dcf410647
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:40:43 +0300

    Add files via upload

---
+++ Практическая работа 4.5/Практическая работа 4.5/Практическая работа 4.5.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    for (double x = 0; x <= 2020; x += 4)
+        cout << "\n\t " << setw(6) << x << " год до нашей эры – високосный";
+
+}
