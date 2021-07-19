4d956f3b91b7fa345d4e151d940b2c3308a47225
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:46:59 +0300

    Add files via upload

---
+++ Лабораторная работа 2_1/Лабораторная работа 2_1/Лабораторная работа 2_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    const int MAX(100);
+    char mass[MAX];
+    int i, c = 0, s = 0;
+    cout << "Введите текст: " << endl;
+    cin.getline(mass, 100);
+    for (i = 0; mass[i] != '\0'; i++)
+        if (mass[i] != ' ' && s == 0) 
+        {
+            c += 1;
+            s = 1;
+        }
+        else
+            if (mass[i] == ' ') 
+                s = 0;
+    cout << endl;
+    cout <<"Предложение состоит из: "<< c <<" слов.";
+}
