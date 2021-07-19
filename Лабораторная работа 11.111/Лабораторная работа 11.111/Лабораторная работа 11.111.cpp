5fadcf96a28aea8c2c2cdb84f590e1b71b24592f
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:18:33 +0300

    Add files via upload

---
+++ Лабораторная работа 11.111/Лабораторная работа 11.111/Лабораторная работа 11.111.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    System::Double carpetPriceSqYd = 27.95; 
+    System::Double roomWidth = 13.5;    
+    System::Double roomLength = 24.75;           
+    const System::Int16 feetPerYard = 3;           
+    System::Double roomWidthYds = roomWidth / feetPerYard; 
+    System::Double roomLengthYds = roomLength / feetPerYard; 
+    System::Double carpetPrice = roomWidthYds * roomLengthYds * carpetPriceSqYd; 
+    Console::WriteLine(L"Room size is {0:F2} yards by {1:F2} yards", roomLengthYds, roomWidthYds); 
+    Console::WriteLine(L"Room area is {0:F2} square yards", roomLengthYds * roomWidthYds); 
+    Console::WriteLine(L"Carpet price is ${0:F2}", carpetPrice);
+    return 0;
+}
