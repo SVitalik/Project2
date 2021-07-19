b2d94a9e4cb444fbd6d974e31bd5aaee35eb0f5e
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:13:39 +0300

    Add files via upload

---
+++ Практическая работа 10.3/Практическая работа 10.3/Практическая работа 10.3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main()
+{
+    char p[16];
+    int ch = 0, i = 0;
+
+    cin >> p;
+    _strrev(p);
+    for (i = 0; i < strlen(p); i++)
+    {
+        if (i == 0)
+        {
+            if (p[i] == '0')
+                ;
+            if (p[i] == '1')
+                ch = 1;
+        }
+        if (i > 0)
+            ch += pow(2 * (p[i] - '0'), i);
+    }
+    cout << hex << ch << "\n";
+    system("pause");
+    return 0;
+}
+
+
+
