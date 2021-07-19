96b42e7cc2a30b244553fc6077b62eec59794528
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:34:15 +0300

    Add files via upload

---
+++ Практическая работа 5.1/Практическая работа 5.1/Практическая работа 5.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    float ax, ay, bx, by, a, b;
+    cout << "Начало координат (0:0)" << endl;
+    cout << "Введите координаты точок А: ";
+    cin >> ax >> ay;
+    cout << "Введите координаты точок В: ";
+    cin >> bx >> by;
+    a = ax * ax + ay * ay;
+    b = bx * bx + by * by;
+    if (a == b)
+        cout << "Они одинаково отдалены от начальной точки: " << endl;
+    else if (a > b)
+        cout << "точка A более отдалена, чем точка В: " << endl;
+    else if (a < b)
+        cout << "точка B более отдалена, чем точка A: " << endl;
+}
+
+
