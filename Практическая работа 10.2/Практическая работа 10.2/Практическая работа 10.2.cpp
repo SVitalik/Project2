2b5f863da9cd280c1e7dba7e2a19a35fab68439b
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:15:12 +0300

    Add files via upload

---
+++ Практическая работа 10.2/Практическая работа 10.2/Практическая работа 10.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int const m = 17;
+	int a[m];
+	int i;
+	float sum;
+	sum = 0;
+	srand(time(0));
+	for (i = 0; i < m; i++)
+	{
+		a[i] = rand() % 100;
+		sum = sum + a[i];
+        cout << "[" << i << "]= "<<a[i]<<endl;
+	}
+	cout<<"Сумма цифр этого масива = "<<sum;
+
+}
