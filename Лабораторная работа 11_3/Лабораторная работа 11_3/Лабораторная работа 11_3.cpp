2f386f8fd9e24be945483c0009f20cf8b0b5b4d9
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:14:42 +0300

    Add files via upload

---
+++ Лабораторная работа 11_3/Лабораторная работа 11_3/Лабораторная работа 11_3.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+
+using namespace System;
+
+int main(array<System::String^>^ args)
+{
+    const Int32 m = 4;
+    Int32 mass[m][m] = { 0 };
+    Int32 r = 0, p = 0;
+    while (r < m)
+    {
+        //Вправо
+        for (Int32 i = r; i < m - r - 1; i++)
+        {
+            p++;
+            mass[r][i] = p;
+        }
+
+        //Вниз
+        for (Int32 i = r; i < m - r - 1; i++)
+        {
+            p++;
+            mass[i][m - r - 1] = p;
+        }
+
+        //Влево
+        for (Int32 i = m - r - 1; i > r; i--)
+        {
+            p++;
+            mass[m - r - 1][i] = p;
+        }
+
+        //Вверх
+        for (Int32 i = m - r - 1; i > r; i--)
+        {
+            p++;
+            mass[i][r] = p;
+        }
+
+        r++;
+    }
+
+    if (m % 2 != 0)
+    {
+        mass[(Int32)(m - 1) / 2][(Int32)(m - 1) / 2] = ++p;
+    }
+    Console::WriteLine(L"Вывод масива: ");
+    Console::WriteLine();
+    for (Int32 i = 0; i < m; i++)
+    {
+        for (Int32 j = 0; j < m; j++)
+        {
+            Console::Write(mass[i][j]);
+            Console::Write("\t");
+            
+        }
+        Console::WriteLine("\t");
+    }
+}
