2f211151e60a4be84a6c95683e531b9001973164
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:54:46 +0300

    Add files via upload

---
+++ ДЗ_1/ДЗ_1/ДЗ_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x;
+    double y;
+    cout << "Введите х: " << endl;
+    cin >> x;
+    int* a = &x;
+    y = tan(*a) + 5 * pow(*a, 3) - 4 * pow(*a, 2);
+    cout << "Ответ = " << y << endl;
+}
+
---
+++ ДЗ_2/ДЗ_2/ДЗ_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    double d, pi = 3.1415;
+    cout << "Введите длину: " << endl;
+    cin >> d;
+    double& a = d;
+    cout <<"Площадь круга " << (a * a) / (4 * pi) << endl;
+}
+
