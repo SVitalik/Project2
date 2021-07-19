f7354197896db66d76aa51113d448ca9645c5ffa
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 15:07:23 +0300

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "RUS");
+    int b = 20000, c = 5, d = 12, e = 1;
+    float a;
+    float f = 0.12;
+    a = b * (e + f) / c;
+    cout << "Размер выплат: ";
+    int ch = _getch();
+    cout << "" << a << " гривень." << endl;
+
+    
+}
---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a, b, c;
+	cout << "Введите трёхзначное число:" << endl;
+	cin >> a;
+	cout << "Введите двухзначное число:" << endl;
+	cin >> b;
+	cout << "Ответ: ";
+	c = a + b;
+	cout << " " << c << endl;
+
+}
