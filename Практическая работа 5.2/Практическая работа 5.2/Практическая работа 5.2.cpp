dc84e4f14ef5a6e1fcd6b4a9d16a4d3432fede54
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:33:43 +0300

    Add files via upload

---
+++ Практическая работа 5.2/Практическая работа 5.2/Практическая работа 5.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    double b, m, n, x, y;
+    cout << "Введите: b, m, n, x" << endl;
+    cin >> b;
+    cin >> m;
+    cin >> n;
+    cin >> x;
+    if (abs(b * m) > x * x)
+        y = sin(b * m + cos(n * x));
+    if (abs(b * m) < x * x)
+        y = cos(b * m - sin(x));
+    if (abs(b * m) <= x * x)
+        y = sqrt(exp(abs(cos(x))) + sqrt(abs(b * m * x)));
+    cout << y << endl;
+ }
