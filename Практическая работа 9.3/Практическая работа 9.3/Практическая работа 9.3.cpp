5592f743424c3994d7623313d908d4644c4aa255
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:20:44 +0300

    Add files via upload

---
+++ Практическая работа 9.3/Практическая работа 9.3/Практическая работа 9.3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <conio.h>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    double c = 0, sar = 0, sge = 0;
+    double D[13] = { 5, -6, 24, -9.8, 15.3, 45, -6.23, 7, 2, -7, 54.1, 12.2, -23 };
+    for (int i = 0; i < 13; i++)
+    {
+        if (D[i] > 0)
+        {
+            c++;
+            sar += D[i];
+            sge *= D[i];
+        }
+    }
+    sar /= c;
+    sge = pow(sge, 1.0 / c);
+    cout << "Средне арифметические елементи: " << sar << endl << "Средне геометрические елементи:" << sge << endl;
+}
+
