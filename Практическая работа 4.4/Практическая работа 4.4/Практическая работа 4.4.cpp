f041085fb49d7c7475f82ed42d945384b39272ae
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:41:32 +0300

    Add files via upload

---
+++ Практическая работа 4.4/Практическая работа 4.4/Практическая работа 4.4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	system("cls");
+	setlocale(LC_ALL, "RUS");
+	double s, r;
+	cout << "Введите стоимость покупки: ";
+	cin >> s;
+	r = s * 0.90;
+	if (s > 1000)
+		cout << "Стоимость с учетом скидки = " << r << endl;
+	else if (r = s)
+		cout << "Скидка не действует" << endl << "Без учета скидки = " << r << endl; system("pause");
+
+}
