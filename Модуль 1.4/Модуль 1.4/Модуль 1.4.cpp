53fe032ef8688e20008eca8a45f76c554deb115a
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:05:54 +0300

    Add files via upload

---
+++ Модуль 1.4/Модуль 1.4/Модуль 1.4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    char indikator = 'n';
+    double x, y, y3;
+    for (;;)
+    {
+        setlocale(LC_ALL, "RUS");
+        cout << "Введите x,y" << endl;
+        cin >> x >> y;
+        y3 = x * x;
+        if ((x <= 0 && y <= abs(x) && y >= y3) || (x > 0 && y <= x && y >= y3))
+            cout << "Да " << endl;
+        else
+            cout << "Нет " << endl;
+        cout << "Еще? " << endl;
+        cin >> indikator;
+        if (indikator == 'n' || indikator == 'N')
+            break;
+
+    }
+
+}
