7bef1a8e79af9a997f0fdb124d2b2cfa1b57f287
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:43:22 +0300

    Add files via upload

---
+++ Лабораторная работа 4_4/Лабораторная работа 4_4/Лабораторная работа 4_4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int Sum(int a)
+{
+    return a ? a % 10 + Sum(a / 10) : 0;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int N;
+    cout << "Введите число: ";
+    cin >> N;
+    cout<< "Результат: " <<Sum(N)<<endl;
+    return 0;
+}
+
