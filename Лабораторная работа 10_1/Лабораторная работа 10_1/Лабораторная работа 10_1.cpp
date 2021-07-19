6abe77e5b914661cafaea738ac8ca966eb7a0bf6
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:29:43 +0300

    Add files via upload

---
+++ Лабораторная работа 10_1/Лабораторная работа 10_1/Лабораторная работа 10_1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    map<char, char> skop{ { '(', ')' }, { '[', ']' }, { '{', '}'} };
+    stack <char> stek;
+    int i = 0;
+    string sh;
+    cout << "Введите скобочным выражением: " << endl;
+    cin >> sh;
+    for (i = 0; i < sh.size(); ++i)
+    {
+        if ('(' == sh[i] || '[' == sh[i] || '{' == sh[i])
+        {
+            stek.push(sh[i]);//добавить елемент в стека
+        }
+        else if (!stek.empty())//проверка
+        {
+            char d = stek.top();//новый елемент
+            stek.pop();//извлечь
+            if (skop[d] != sh[i])
+            {
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (i == sh.size() && stek.empty())
+    {
+        cout << "Введено правильно!" << endl;
+    }
+    else
+    {
+        cout << "Введено неправильно!" << endl;
+    }
+}
