73c6cd8331adc3a5d3a40791a0e1781dfc59aa59
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:18:44 +0300

    Add files via upload

---
+++ Практическая работа 9.5/Практическая работа 9.5/Практическая работа 9.5.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int x = 0, y = 0;
+    double k[14];
+    for (int i = 0; i < 14; i++)
+    {
+        cin >> k[i];
+    }
+    for (int a = 0; a < 14; a++)
+    {
+        if (k[a] < x)
+        {
+            y++;
+        }
+        else
+        {
+            x = k[a];
+        }
+    }
+    cout << endl;
+    if (y <= 0)
+    {
+        cout << "Упорядкований" << endl;
+    }
+    else
+    {
+        cout << "Не упорядкований" << endl;
+    }
+    cout << endl;
+    cout << endl;
+}
+
