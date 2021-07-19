3266ad23e6bcbf89417481acf74ed103623d85e4
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:24:14 +0300

    Add files via upload

---
+++ Практическая работа 8.3/Практическая работа 8.3/Практическая работа 8.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+    double y;
+    for (double i = -2; i <= 2; i += 0.4)
+    {
+        if (i > 0)
+        {
+            y = pow(pow(i, 3), 1.0 / 5);
+        }
+        else if (i <= 0)
+        {
+            for (int j = 1; j <= 3; j++)
+            {
+                y = pow(i, 2) + j * (pow(i, j) / j);
+            }
+        }
+        cout << setw(10) << "i = " << i;
+        cout << setw(25) << "y = " << y << endl;
+
+    }
+}
+
