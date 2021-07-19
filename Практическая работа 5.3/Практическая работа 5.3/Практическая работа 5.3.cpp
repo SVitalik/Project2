c7ed74489ed4076be5ee86e88db5b6c4d81ad93f
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:32:45 +0300

    Add files via upload

---
+++ Практическая работа 5.3/Практическая работа 5.3/Практическая работа 5.3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int d;
+    const char s[][12] = { "понедельник", "вторник", "среда", "четверг", "пятница", "субота", "воскресение" };
+    cout << "Введите день недели: ";
+    cin >> d;
+    if (d % 7 != 0)
+        cout << s[d % 7 - 1] << endl;
+    else cout << s[6] << endl;
+}
