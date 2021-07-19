5637cffaac954460a925e844f17bc8d9e87ef6d4
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 15:11:44 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "RUS");
+    const float pi = 3.14159265f;
+    float h, r, v;
+    cout << "Введите данные: " << endl;
+    cout << "h= ";
+    cin >> h;
+    cout << "r= ";
+    cin >> r;
+    v = pi * (r * r) * h;
+    cout << "Площадь поверхности прямого целиндра: " << v << endl;
+}
+
+
---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "RUS");
+    const float pi = 3.14159265f;
+    float h, r, s;
+    cout << "Введите данные: " << endl;
+    cout << "h= ";
+    cin >> h;
+    cout << "r= ";
+    cin >> r;
+    s = 2 * pi * r * (h + r);
+    cout << "Объем прямого цилиндра: " << s << endl;
+}
