265888fd86b0049d133cc619cfddd53d05e4c744
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:52:31 +0300

    Add files via upload

---
+++ Практическая 3.1/Практическая 3.1/Практическая 3.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+
+void main()
+{
+    system("cls");
+    setlocale(LC_ALL, "RUS");
+    double a, b, x, z, f;
+    cout << "Введите значение a,b,x,z: "<<endl;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "x = ";
+    cin >> x;
+    cout << "z = ";
+    cin >> z;
+    f = (a * pow(sin(abs(b * x)), 2) - sqrt(24 * z)) /
+        (pow(-x + 13, 1 / 3) - b * log(z * z + 4));
+    cout << "Результат f = " << f << endl;
+    int ch = _getch();
+}
