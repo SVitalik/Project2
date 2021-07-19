8310351d3dc2fdaa9f1ec5bac2b7a32ba2d47119
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:44:07 +0300

    Add files via upload

---
+++ Лабораторная работа 4_2/Лабораторная работа 4_2/Лабораторная работа 4_2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void func(int a, int b) 
+{
+	if (a < b) 
+	{
+		for (int i = a; i <= b; i++)
+			cout << i<<endl;
+	}
+	else 
+	{
+		for (int i = a; i >= b; i--)
+			cout << i << endl;
+	}
+}
+int main() 
+{
+	setlocale(LC_ALL, "rus");
+	int num1, num2;
+	cout << "Введите число A: " << endl;
+	cin >> num1;
+	cout << "Введите число B: " << endl;
+	cin >> num2;
+	cout << endl;
+	func(num1, num2);
+}
