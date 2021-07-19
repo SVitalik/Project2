9664b89cef5c5bfabc1268bf8a70de4fb7b5767b
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:53:50 +0300

    Add files via upload

---
+++ ДЗ_3/ДЗ_3/ДЗ_3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int const n = 10;
+    int A[n];
+    int i, s = 0, d = 0, max;
+    int* b = A;
+    for (i = 0; i <= n - 1; i++)
+    {
+        cout << "A[" << i << "]= ";
+        cin >> b[i];
+    }
+    max = b[0];
+    cout << "Положительнные елементы: " << endl;
+    for (i = 0; i <= n - 1; i++)
+    {
+        if (b[i] < max)
+        {
+            max = b[i];
+            d += b[i];
+        }
+        if (b[i] > 0)
+        {
+            s += b[i];
+            cout << "A[" << i << "]= " << b[i] << endl;
+        }
+    }
+    cout << "Максимально отрицательный елемент: "<< max << endl;
+    cout << "Сумма отрицательных елементов масива: " << d << endl;
+
+
+}
+
---
+++ ДЗ_4/ДЗ_4/ДЗ_4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(0));
+    int const n = 10;
+    /*int a[n];*/
+    int* b = new int [n];
+    int i, s = 0, min;
+    double d = 1;
+    cout << "Рандомный массив: " << endl;
+    for (i = 0; i <= n - 1; i++)
+    {
+        b[i] = rand() % 101 - 50;
+        cout << "A[" << i << "]= " << b[i] << endl;
+    }
+    cout << endl;
+    min = b[0];
+    cout << "Положительнные елементы: " << endl;
+    for (i = 0; i <= n - 1; i++)
+    {
+        if (b[i] > min)
+        {
+            min = b[i];
+        }
+        if (b[i] > 0)
+        {
+            s += b[i];
+            cout << "A[" << i << "]= " << b[i] << endl;
+        }
+        d *= b[i];
+    }
+    cout << endl;
+    cout << "Максимально положительных елемент: " << min << endl;
+    cout << "Произведение элементов массива: " << d << endl;
+    
+
+}
