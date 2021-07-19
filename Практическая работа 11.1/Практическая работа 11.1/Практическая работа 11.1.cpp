c1812f60479760239cd6afe7ce5ae82915b70132
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:07:22 +0300

    Add files via upload

---
+++ Практическая работа 11.1/Практическая работа 11.1/Практическая работа 11.1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    const int n = 5;
+    const int m = 6;
+    double sum = 0;
+    int a[n][m], b[n][m] = { 0 }, k = 0;
+    for (int str = 0; str < n; str++)
+    {
+        for (int stlb = 0; stlb < m; stlb++)
+        {
+            a[str][stlb] = rand() % 100;
+            cout << setw(4) << a[str][stlb];
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << endl;
+    for (int str = 0; str < n; str++)
+    {
+        for (int stlb = 0; stlb < m; stlb++)
+        {
+            if (a[str][stlb] > 0)
+                sum += a[str][stlb];
+
+        }
+        cout << "Sum " << str + 1 << ": " << sum / m << endl;
+    }
+
+}
+
