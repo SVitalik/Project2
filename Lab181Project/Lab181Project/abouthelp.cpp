64b3daabf7652cee9989276dde0a86ff43f1f216
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:58:36 +0300

    Add files via upload

---
+++ Lab181Project/Lab181Project/abouthelp.cpp
@@ -0,0 +1,2 @@
+#include "abouthelp.h"
+
---
+++ Lab181Project/Lab181Project/abouthelp.h
@@ -0,0 +1,80 @@
+#pragma once
+
+namespace Lab181Project {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ abouthelp
+	/// </summary>
+	public ref class abouthelp : public System::Windows::Forms::Form
+	{
+	public:
+		abouthelp(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~abouthelp()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(abouthelp::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(330, 300);
+			this->label1->TabIndex = 0;
+			this->label1->Text = resources->GetString(L"label1.Text");
+			// 
+			// abouthelp
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(354, 318);
+			this->Controls->Add(this->label1);
+			this->Name = L"abouthelp";
+			this->Text = L"abouthelp";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
