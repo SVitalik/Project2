07ee31d417b21a6afdd6c3f94bc424a01b49a0c7
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:56:48 +0300

    Add files via upload

---
+++ Практичекая работа 7.5/Практичекая работа 7.5/Практичекая работа 7.5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    double s, c, t, ct, x;
+    for (x = 0; x <= 180; x += 10)
+    {
+        s = sin(x * 3.14159 / 180);
+        c = cos(x * 3.14159 / 180);
+        t = tan(x * 3.14159 / 180);
+        ct = 1 / t;
+        cout << "x = " << setw(4) << x << setw(8) << "sin = " << s << endl;
+        cout << "x = " << setw(4) << x << setw(8) << "cos = " << c << endl;
+        cout << "x = " << setw(4) << x << setw(8) << "tan = " << t << endl;
+        cout << "x = " << setw(4) << x << setw(8) << "ctg = " << ct << endl;
+        cout << endl;
+    }
+}
+
