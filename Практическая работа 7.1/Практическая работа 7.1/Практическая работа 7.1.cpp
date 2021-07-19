18d563f14b6abf42130e67f9d9eec6fc409b848e
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:32:02 +0300

    Add files via upload

---
+++ Практическая работа 7.1/Практическая работа 7.1/Практическая работа 7.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+	double a, b, x, y;
+	a = 4.6;
+	b = 6.8;
+	for (x = 1.2; x <= 3; x += 0.2)
+	{
+		y = b * x + a / pow(log(a * x), 2) + sqrt(b);
+		cout << "x = " << setw(3) << x << setw(8) << "y = " << y << endl;
+	}
+}
+
+
