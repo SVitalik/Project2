006d85757f0b241fed9b529757cd1c9377b27f12
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:20:09 +0300

    Add files via upload

---
+++ Практическая работа 9.4/Практическая работа 9.4/Практическая работа 9.4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int const n = 10;
+    int A[n];
+    int i;
+    int s = 0, d = 0, f = 0, g = 0;
+    for (i = 0; i <= n - 1; i++)
+    {
+        cout << "A[" << i << "]= ";
+        cin >> A[i];
+    }
+    cout << " Количеству парных элементов, кратных 3: " << endl;
+    for (i = 0; i <= n - 1; i++)
+    {
+        if (A[i] % 3 == 0)
+        {
+            d++;
+            cout << "кратных на 3 A[" << i << "]" << endl;
+        }
+    }
+}
