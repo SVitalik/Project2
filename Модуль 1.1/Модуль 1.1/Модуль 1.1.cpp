236573485d7390b2ec0d588ab0fe96870786754e
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 14:07:03 +0300

    Add files via upload

---
+++ Модуль 1.1/Модуль 1.1/Модуль 1.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+void main()
+{
+    system("cls");
+    setlocale(LC_ALL, "RUS");
+    double x,a;
+    cout << "Введите значение x: " << endl;
+    cout << "x = ";
+    cin >> x;
+    a = (sin(x) - cos(pow(x, 3)))*(sin((x * x - 4,2)) + 4,27);
+    cout << "Результат a = " << a << endl;
+    int ch = _getch();
+}
+
+//(pow(sin(x) - cos(x), 3 * sin))* (x* x - 4, 2) + 4, 27;
---
+++ Модуль 1.2/Модуль 1.2/Модуль 1.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+void main()
+{
+    system("cls");
+    setlocale(LC_ALL, "RUS");
+    double x, a, b, d;
+    d = 299792;
+    b = d*60;
+    x = b * 60;
+    a = x * 24;
+    cout << "Растояние которое проедалевает свет за час: " << x << endl;
+    cout << "Растояние которое проедалевает свет за день: " << a << endl;
+    //cout << "Введите значение s: " << endl;
+    //cout << "s = ";
+    //cin >> s;
+
+
+}
