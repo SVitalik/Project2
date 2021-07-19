48c5c6d5752b2798e5c411b6db874f35ae9fd949
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-07-19 13:28:45 +0300

    Add files via upload

---
+++ Практическая работа 7.4/Практическая работа 7.4/Практическая работа 7.4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int a, b, x;
+    a = 0;
+    b = 1;
+    cout << "Введите число х: " << endl;
+    cin >> x;
+    while (x != 0)
+    {
+        a = a + x;
+        b = b * x;
+        cin >> x;
+    }
+    cout << "Сумма = " << a << endl;
+    cout << "Добуток = " << b << endl;
+}
+
