9636c26b7da62e54d7eb61bf7815cd2114df4b81
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-12-22 17:28:48 +0200

    Add files via upload

---
+++ 2 курс/Лабораторная работа 11_2/Лабораторная работа 11_2/Лабораторная работа 11_2.cpp
@@ -0,0 +1,33 @@
+﻿//Ввести с клавиатуры три числа. Вывести на экран среднее по значению из них
+#include "pch.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Введите три числа:");
+    double a = Double::Parse(Console::ReadLine());
+    double b = Double::Parse(Console::ReadLine());
+    double c = Double::Parse(Console::ReadLine());
+    Console::WriteLine(L"Среднее значение:");
+    if ((a > b && a < c) || (a < b && a > c)) 
+    {
+        Console::WriteLine(a);
+    } 
+    if ((b > a && b < c) || (b < a && b > c)) 
+    {
+        Console::WriteLine(b);
+    }   
+    if ((c > a && c < b) || (c < a && c > b)) 
+    {
+        Console::WriteLine(c);
+    } 
+    if (a == b && b == c && c == a) 
+    {
+        Console::WriteLine(a);
+    } 
+    if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a)) 
+    {
+        Console::WriteLine(b);
+    }
+}
