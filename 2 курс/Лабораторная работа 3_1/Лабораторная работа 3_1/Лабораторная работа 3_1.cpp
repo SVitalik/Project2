7685dc4d904f65a5d8e7879a1cc80468cd1bc97b
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-12-22 17:09:29 +0200

    Add files via upload

---
+++ 2 курс/Лабораторная работа 3_1/Лабораторная работа 3_1/Лабораторная работа 3_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double fun(int a, int b);
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    double y = 0.0, CHa = 0.0, CHb = 0.0;
+    cout << "Введите число а: " << endl;
+    cin >> CHa;
+    cout << "Введите число b: " << endl;
+    cin >> CHb;
+    cout << "Найменьшее общее краткое: " << fun(CHa, CHb);
+}
+
+
+double fun(int a, int b)
+{
+    while (a != 0 && b != 0) {
+        if (a > b) {
+            a = a % b;
+        }
+        else {
+            b = b % a;
+        }
+    }
+    return a + b;
+}
+
+
+
+
+
+
