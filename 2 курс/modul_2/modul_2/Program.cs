761187b980d5fd98ab3429c4828b11bde8d0535d
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-12-22 16:45:09 +0200

    Add files via upload

---
+++ 2 курс/modul_2/modul_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul_2
+{
+    namespace ConsoleApplication1
+    {
+        class A
+        {
+            public void WriteMessage()
+            {
+                Console.Write("A.Write ");
+            }
+        }
+
+        class B : A
+        {
+            public new void WriteMessage()
+            {
+                Console.Write("B.Write ");
+            }
+        }
+
+        class C : B
+        {
+            public new void WriteMessage()
+            {
+                Console.Write("C.Write ");
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                A x = new B();
+                x.WriteMessage();
+                x = new C();
+                x.WriteMessage();
+            }
+        }
+    }
+}
