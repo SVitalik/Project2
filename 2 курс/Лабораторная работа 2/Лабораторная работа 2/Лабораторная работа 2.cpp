15c8a3cb3da93136e45479459d29e8fdc2c024a2
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-12-22 17:02:07 +0200

    Add files via upload

---
+++ 2 курс/Лабораторная работа 2/Лабораторная работа 2/Лабораторная работа 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	const int MAX(100);
+	char mass[MAX];
+	cout << "Введите текст: " << endl;
+	cin.getline(mass, 100);
+	char MaS = mass[0];
+	mass[0] = mass[strlen(mass) - 1];
+	mass[strlen(mass) - 1] = MaS;
+	/*swap(mass[0], mass[strlen(mass) - 1]);*/
+	cout << mass;
+}
