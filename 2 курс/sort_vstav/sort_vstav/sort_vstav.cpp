ffb75135d18844e881f7f37ba8c7df6df5c227d4
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-12-22 16:56:48 +0200

    Add files via upload

---
+++ 2 курс/sort_vstav/sort_vstav/sort_vstav.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	const int n = 100;
+	int a[n];
+	int s = 0;
+	int i, j, c;
+    cout << "Введите масив: " << endl;
+	cin >> c;
+	for (i = 0; i < c; i++)
+	{
+		cout << "Число: " << endl;
+		cin >> a[i];
+	}
+	for (i = 1; i < c; i++)
+	{
+		s = a[i];
+		for (j = i - 1; j >= 0 && a[j] > s; j--)
+			a[j + 1] = a[j];
+		a[j + 1] = s;
+		
+	}
+	cout << endl;
+	for (i = 0; i < c; i++)
+	{
+        cout << a[i] << '\t';
+		cout << endl;
+	}
+	
+}
+
+
