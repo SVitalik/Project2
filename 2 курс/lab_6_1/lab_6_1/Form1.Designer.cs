7fcc9b6f62fd6908e3363f8cfa399cd98ba63596
SVitalik <87321397+SVitalik@users.noreply.github.com>
2021-12-22 16:17:25 +0200

    Add files via upload

---
+++ 2 курс/lab_6_1/lab_6_1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace lab_6_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(358, 42);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 334);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ 2 курс/lab_6_1/lab_6_1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_6_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //double[,] mass = new double[5, 5];
+            Random rand = new Random();
+
+            //int[][] M = new int[5][]; 
+            //M[0] = new int[3];
+            //M[1] = new int[4]; 
+            //M[2] = new int[5];
+            //M[3] = new int[3];
+            //M[4] = new int[2];
+
+            double[][] Z = new double[5][];
+
+            double[][] P = new double[5][];
+            P[0] = new double[5];
+            P[1] = new double[5];
+            P[2] = new double[5];
+            P[3] = new double[5];
+            P[4] = new double[5];
+
+
+
+
+            for (int i = 0; i < P.Length; i++)
+            {
+                for(int j = 0; j < P[i].Length; j++)
+                {
+                    P[i][j] = rand.Next(0, 20);
+                }
+            }
+
+
+            dataGridView1.RowCount = P.Length;
+            dataGridView1.ColumnCount = 5; // тут с помощью кода не знаю как вычислять, разве что циклом узнавать максимальное кол-во элементов в каждом "под-массиве"
+
+            for (int i = 0; i < P.GetLength(0); i++)
+            {
+                for (int j = 0; j < P[i].Length; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = P[i][j];
+                    dataGridView1.Columns[i].Width = 30;
+                    dataGridView1.Columns[i].Name = $"P{i}";
+                }
+            }
+
+            dataGridView2.RowCount = Z.Length;
+            dataGridView2.ColumnCount = 5; // тут с помощью кода не знаю как вычислять, разве что циклом узнавать максимальное кол-во элементов в каждом "под-массиве"
+
+            for (int i = 0; i < P.GetLength(0); i++)
+            {
+                for (int j = 0; j < P[i].Length; j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = P[i][j];
+                    dataGridView2.Columns[i].Width = 30;
+                    dataGridView2.Columns[i].Name = $"Z{i}";
+                }
+            }
+
+        }
+    }
+}
